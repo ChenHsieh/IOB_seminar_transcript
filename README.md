7a1286e7dca4976de8f41002778ca53563cb3910
chen <chen.hsieh@uga.edu>
2023-04-26 16:44:13 -0400

    timestring & youtube link

--- README.md
+++ README.md
@@ -8,5 +8,4 @@ All the transcripts of IOB Spring seminars as of 4/23 are in the `out` folder.
 
 The scripts used to generate the transcripts are in the `scripts` folder. I first used the `yt_dlp.sh` to download the sound from youtube video into `mp3` format. Then used the `prep_whisper_job.py` to generate commands for each sound file. Finally, I used the generated `cmd.sh` to submit the job to UGA's sapelo2 cluster.
 
-## Posts
 
--- scripts/prep_jekyll_page.py
+++ scripts/prep_jekyll_page.py
@@ -11,18 +11,17 @@ for video_title,url in zip(videos['video_title'], videos['url']):
         f.write('categories: Spring2023\n')
         f.write('---\n')
         f.write('\n')
-        url = url.replace("watch?v=", "embed/")
-        f.write(f'<iframe width="560" height="315" src="{url}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
+        embed_url = url.replace("watch?v=", "embed/")
+        f.write(f'<iframe width="560" height="315" src="{embed_url}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
         f.write('\n\n')
         # read the tsv file in out with the same name as the video title
         transcripts = pd.read_csv(f'./out/{video_title}.tsv', sep="\t")
         # write the contents of the tsv file to the markdown file
         f.write(f"## Transcript\n\n")
-        f.write("[ start - end ]\ttext\n\n")
-        for sn,info in transcripts.iterrows():
-            f.write(f"[ {info['start']} - {info['end']}]\t{info['text']}")
-            f.write('\n\n')
         
+        for sn,info in transcripts.iterrows():
 
-
+            time_string = '{:02d}:{:02d}:{:06.3f}'.format((info['start'] // 3600000) % 24, (info['start'] // 60000) % 60, (info['start'] % 60000) / 1000)
+            f.write(f"![{time_string}]({url}&t={int(info['start']/1000)})\t{info['text']}")
+            f.write('\n\n')
 
